68379e51bd040751e5de643608727bc104d6a2ab
garrafapet157 <53275513+garrafapet157@users.noreply.github.com>
2023-03-15 21:38:47 -0300

    novo projeto

---
+++ bytebank/bytebank-composto/src/Conta.java
@@ -0,0 +1,36 @@
+/**
+ * Conta
+ * agencia
+ * numero
+ * titular
+ */
+public class Conta {
+
+    public double saldo;
+    public int agencia;
+    int numero;
+    Cliente titular;
+
+    public void deposita(double valor) {
+        this.saldo += valor;
+
+    }
+
+    public boolean saca(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean transfere(double valor, Conta destino) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            destino.deposita(valor);
+            return true;
+        }
+        return false;
+    }
+
+}
